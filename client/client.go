176748fc40f89db39daf67de12d0320d70077b10
viniciusol263 <viniciuscme2@gmail.com>
2022-09-13 16:20:58 -0300

    GRPC example concluded

---
+++ client/client.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"context"
+	"grpc/protobuf/pb"
+	"io"
+	"log"
+	"math"
+	"sort"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type TriangleStruct struct {
+	triangle *pb.Triangle
+	side_a   float64
+	side_b   float64
+	side_c   float64
+}
+
+const (
+	NonTriangleError = -1
+)
+
+func NewTriangleStruct(triangle *pb.Triangle) (*TriangleStruct, error) {
+	ts := new(TriangleStruct)
+
+	slice_sizes := []*float64{&ts.side_a, &ts.side_b, &ts.side_c}
+
+	ts.side_a = math.Sqrt(math.Pow(float64(triangle.Vertice_2.X)-float64(triangle.Vertice_1.X), 2) + math.Pow(float64(triangle.Vertice_2.Y)-float64(triangle.Vertice_1.Y), 2))
+	ts.side_b = math.Sqrt(math.Pow(float64(triangle.Vertice_3.X)-float64(triangle.Vertice_2.X), 2) + math.Pow(float64(triangle.Vertice_3.Y)-float64(triangle.Vertice_2.Y), 2))
+	ts.side_c = math.Sqrt(math.Pow(float64(triangle.Vertice_1.X)-float64(triangle.Vertice_3.X), 2) + math.Pow(float64(triangle.Vertice_1.Y)-float64(triangle.Vertice_3.Y), 2))
+	ts.triangle = triangle
+
+	sort.Slice(slice_sizes, func(i, j int) bool {
+		return *slice_sizes[i] > *slice_sizes[j]
+	})
+
+	if *slice_sizes[0] <= *slice_sizes[1]+*slice_sizes[2] {
+		log.Println("It's a triangle!")
+		return ts, nil
+	}
+	return ts, nil
+}
+
+func executeCreateTriangle(listPoints []*pb.Point, client pb.RendererClient) (*TriangleStruct, error) {
+	stream, err := client.CreateTriangle(context.Background())
+	if err != nil {
+		log.Fatalf("CreateTriangle Failed %v", err)
+	}
+	log.Printf("Sending points from listOfPoints to CreateTriangle\n")
+	for _, point := range listPoints {
+		err := stream.Send(point)
+		if err != nil {
+			log.Fatalf("Error has occoured as points was being sent %v", err)
+		}
+
+	}
+	triangle, err := stream.CloseAndRecv()
+	if err != nil {
+		return &TriangleStruct{}, err
+	}
+	triangleStructure, _ := NewTriangleStruct(triangle)
+
+	log.Printf("Triangle created %+v, triangle itself: %+v\n", triangleStructure, triangleStructure.triangle)
+	return triangleStructure, nil
+}
+
+func getPolygonList(client pb.RendererClient) ([]*pb.Polygon, error) {
+	var polygonList []*pb.Polygon
+
+	stream, err := client.ListOfPolygons(context.Background(), &pb.Void{})
+	if err != nil {
+		return []*pb.Polygon{}, err
+	}
+	for {
+		polygon, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return []*pb.Polygon{}, err
+		}
+		polygonList = append(polygonList, polygon)
+		log.Printf("Received polygon %+v\n", polygon)
+	}
+	log.Println("Finished listing polygons!")
+	return polygonList, nil
+}
+
+func getTriangleList(client pb.RendererClient) ([]*pb.Triangle, error) {
+	var triangleList []*pb.Triangle
+	stream, err := client.ListOfTriangles(context.Background(), &pb.Void{})
+	if err != nil {
+		return []*pb.Triangle{}, err
+	}
+	for {
+		triangle, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return []*pb.Triangle{}, err
+		}
+		triangleList = append(triangleList, triangle)
+		log.Printf("Received triangle %+v\n", triangle)
+	}
+
+	log.Println("Finished listing triangles!")
+	return triangleList, nil
+}
+
+func executeCreatePolygon(client pb.RendererClient, listTriangle []*pb.Triangle) error {
+	var polygonList []*pb.Polygon
+	waitc := make(chan struct{})
+
+	stream, err := client.CreatePolygons(context.Background())
+	if err != nil {
+		return err
+	}
+	go func() {
+		for {
+			polygon, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("Error %v", err)
+			}
+			polygonList = append(polygonList, polygon)
+			log.Printf("Polygon %+v received!\n", polygon)
+		}
+	}()
+
+	for _, triangle := range listTriangle {
+		err := stream.Send(triangle)
+		if err != nil {
+			return err
+		}
+		log.Printf("Triangle is sent %+v\n", triangle)
+	}
+	stream.CloseSend()
+	<-waitc
+	return nil
+}
+func executeGetPolyTriangles(client pb.RendererClient, in *pb.Polygon) error {
+	stream, err := client.GetPolyTriangles(context.Background(), in)
+	if err != nil {
+		return err
+	}
+	for {
+		triangle, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("Polygon %s has triangles %v\n", in.PolyName, triangle)
+	}
+
+	return nil
+}
+
+func main() {
+	//	Starts connection with localhost on port 8081 utilizing dummy unsecure credentials
+	conn, err := grpc.Dial(":8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Dial failed! %v", err)
+	}
+	defer conn.Close()
+
+	//	Instantiates and initializes the client-side of the Renderer Service
+	client := pb.NewRendererClient(conn)
+
+	//	Creates 5 Triangles objects using mocked list of Points
+	for counter := 0; counter < len(listOfPoints); counter += 3 {
+		executeCreateTriangle(listOfPoints[counter:], client)
+	}
+
+	//	Gets a list of triangles currently registered on the server
+	triangleList, err := getTriangleList(client)
+	if err != nil {
+		log.Fatalf("Error Occoured %v", triangleList)
+	}
+
+	//	Creates 2 Polygons(Each with 2 Triangles) using previously generated triangles
+	err = executeCreatePolygon(client, triangleList)
+	if err != nil {
+		log.Fatalf("Error Occoured %v", err)
+	}
+
+	//	Gets a list of polygons currently registered on the server
+	polygonList, err := getPolygonList(client)
+	if err != nil {
+		log.Fatalf("Error Occoured %v", err)
+	}
+
+	//	Listing all triangles in a specific polygon
+	for _, polygon := range polygonList {
+		err := executeGetPolyTriangles(client, polygon)
+		if err != nil {
+			log.Fatalf("Error Occoured %v", err)
+		}
+	}
+	//
+	// Searching for a unique Point inside all Polygons and Triangles, and returns the containing triangle
+	triangle, err := client.SearchPoint(context.Background(), listOfPoints[1])
+	if err != nil {
+		log.Fatalf("Error Occoured %v", err)
+	}
+	log.Printf("Point %v is a vertice on %v triangle\n", listOfPoints[1], triangle)
+
+}
+
+var listOfPoints = []*pb.Point{
+	{X: 2.4, Y: 1.3},
+	{X: 5.2, Y: 3.1},
+	{X: 8.5, Y: 9.2},
+	{X: 75.3, Y: 23.1},
+	{X: 86.4, Y: 44.5},
+	{X: 12.3, Y: 67.4},
+	{X: 62.4, Y: 23.86},
+	{X: 34.4, Y: 18.35},
+	{X: 96.4, Y: 78.8},
+	{X: 23.4, Y: 37.5},
+	{X: 77.4, Y: 64.8},
+	{X: 59.4, Y: 89.1},
+	{X: 46.4, Y: 76.2},
+	{X: 23.4, Y: 54.6},
+	{X: 67.4, Y: 34.87},
+}
---
+++ server/server.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"grpc/protobuf/pb"
+	"io"
+	"log"
+	"net"
+	"strconv"
+
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+)
+
+type RendererServer struct {
+	pb.UnimplementedRendererServer
+	listTriangles []*pb.Triangle
+	listPolygons  []*pb.Polygon
+	triangleCount int
+}
+
+const trianglesPerPolygon = 2
+
+func (r *RendererServer) SearchPoint(ctx context.Context, p *pb.Point) (*pb.Triangle, error) {
+	for _, polygon := range r.listPolygons {
+		for _, triangle := range polygon.Triangles {
+			if proto.Equal(triangle.Vertice_1, p) ||
+				proto.Equal(triangle.Vertice_2, p) ||
+				proto.Equal(triangle.Vertice_3, p) {
+				return triangle, nil
+			}
+		}
+	}
+	emptyTriangle := &pb.Triangle{
+		TrId:      int32(r.triangleCount),
+		Vertice_1: &pb.Point{},
+		Vertice_2: &pb.Point{},
+		Vertice_3: &pb.Point{},
+	}
+	return emptyTriangle, nil
+}
+
+func (r *RendererServer) CreatePolygons(stream pb.Renderer_CreatePolygonsServer) error {
+	var trianglesOnPolygon []*pb.Triangle
+	var polygonCounter int
+	for {
+		triangle, err := stream.Recv()
+		if err == io.EOF {
+			r.listPolygons = append(r.listPolygons, &pb.Polygon{
+				PolyName:     "Polygon " + strconv.Itoa(polygonCounter),
+				NumTriangles: int32(len(trianglesOnPolygon)),
+				Triangles:    trianglesOnPolygon,
+			})
+			err := stream.Send(r.listPolygons[polygonCounter])
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		trianglesOnPolygon = append(trianglesOnPolygon, triangle)
+		if len(trianglesOnPolygon) == trianglesPerPolygon {
+			r.listPolygons = append(r.listPolygons, &pb.Polygon{
+				PolyName:     "Polygon " + strconv.Itoa(polygonCounter),
+				NumTriangles: trianglesPerPolygon,
+				Triangles:    append([]*pb.Triangle{}, trianglesOnPolygon...),
+			})
+			err := stream.Send(r.listPolygons[polygonCounter])
+			if err != nil {
+				return err
+			}
+			polygonCounter++
+			trianglesOnPolygon = nil
+		}
+	}
+}
+
+func (r *RendererServer) CreateTriangle(stream pb.Renderer_CreateTriangleServer) error {
+	var threePointsCounter int
+	var triangleVertices []*pb.Point
+	for {
+		point, err := stream.Recv()
+		if err == io.EOF {
+			if threePointsCounter < 3 {
+				stream.SendAndClose(&pb.Triangle{
+					TrId:      int32(r.triangleCount),
+					Vertice_1: &pb.Point{},
+					Vertice_2: &pb.Point{},
+					Vertice_3: &pb.Point{},
+				})
+			}
+			return err
+		}
+
+		if err != nil {
+			return err
+		}
+
+		threePointsCounter++
+		triangleVertices = append(triangleVertices, point)
+		if threePointsCounter == 3 {
+			threePointsCounter = 0
+			r.listTriangles = append(r.listTriangles, &pb.Triangle{
+				TrId:      int32(r.triangleCount),
+				Vertice_1: triangleVertices[0],
+				Vertice_2: triangleVertices[1],
+				Vertice_3: triangleVertices[2],
+			})
+			err := stream.SendAndClose(r.listTriangles[r.triangleCount])
+			r.triangleCount++
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+}
+
+func (r *RendererServer) GetPolyTriangles(p *pb.Polygon, stream pb.Renderer_GetPolyTrianglesServer) error {
+	for _, polygon := range r.listPolygons {
+		if proto.Equal(p, polygon) {
+			for _, triangle := range polygon.Triangles {
+				err := stream.Send(triangle)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+	}
+	return nil
+}
+
+func (r *RendererServer) ListOfTriangles(v *pb.Void, stream pb.Renderer_ListOfTrianglesServer) error {
+	for _, triangle := range r.listTriangles {
+		err := stream.Send(triangle)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *RendererServer) ListOfPolygons(v *pb.Void, stream pb.Renderer_ListOfPolygonsServer) error {
+	for _, polygon := range r.listPolygons {
+		err := stream.Send(polygon)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatalf("Error has occoured %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterRendererServer(grpcServer, &RendererServer{})
+	grpcServer.Serve(lis)
+}
